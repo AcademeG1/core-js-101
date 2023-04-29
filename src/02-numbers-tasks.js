4327c2eb982e86d7cd94d749ec00e6049ad417a4
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-04-29 12:36:50 +0300

    feat: added solution task isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -201,8 +201,19 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const numLength = Math.sqrt(n);
+  if (n === 2) {
+    return true;
+  }
+  let i = 2;
+  while (i <= numLength) {
+    if (n % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
 }
 
 /**
